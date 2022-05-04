df40315d1d8078619900906847886b2e8244cab6
SebastienGaille <57944221+SebastienGaille@users.noreply.github.com>
2022-05-04 13:39:10 +0200

    ajout page menu
    ajout d'animation css ainsi que la suppression du head de la
    index.php

--- public/index.php
+++ public/index.php
@@ -16,20 +16,5 @@ $isConnect = isset($_SESSION['username']);
 if(!$isConnect&& $page !='inscription'  && $page !='creation' && $page !='connection'  ){
     $page ="login";
 }
-
-echo $page;
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-    <?php require_once "../private/pages/{$page}.php"; ?>
+require_once "../private/pages/{$page}.php"; 
    
-</body>
-</html>

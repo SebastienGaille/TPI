589364e5f642346e4eacb95b7a3c249af3c076a5
SebastienGaille <57944221+SebastienGaille@users.noreply.github.com>
2022-05-04 13:40:56 +0200

    ajout page menu
    ajout d'animation css ainsi que la suppression du head de la
    index.php

--- private/pages/menu.php
+++ private/pages/menu.php
@@ -6,14 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>High-Way-Menu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/css/menu.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>
-    <h1 class="display-1">High-Way</h1>
-    <div class>
+        <div class="container">
+            <div class ="title-menu">
+            <h1 class="display-1" id="gameName">High Way </h1>
+            <h1 class="display-1" id="pseudo">Pseudo : <?php echo $_SESSION['username'];?></h1>
+            </div>
+            
+            <div class="row p-3">
+                <div class="col-12 col-md-6">
+                    <h1 class="display-5">
+                        <a href="index.php?page=game" class="link">Jouer</a>
+                    </h1>
+                    <h1 class="display-5">
+                        <a href="index.php?page=ranking" class="link">Classement</a>
+                    </h1>
+                    <h1 class="display-5">
+                        <a href="index.php?page=logout" class="link">Déconnexion</a>
+                    </h1>
 
-    </div>
+                </div>
+
+            </div>
+        </div>
+
+
     </header>
     <main>
 

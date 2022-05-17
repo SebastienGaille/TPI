357b0c99c48e1223ccba94937b4013bd3451d1d2
sebastienG <sebastien.gll@eduge.ch>
2022-05-17 16:42:03 +0200

    correction du systemde de question

--- private/pages/selectQuestion.php
+++ private/pages/selectQuestion.php
@@ -1,36 +1,36 @@
 <?php
+//cette page est inutile 
+// $idquestion=$_SESSION['idquestion'];
+// require_once __DIR__ . '/../myDB.php';
 
-$idquestion=$_SESSION['idquestion'];
-require_once __DIR__ . '/../myDB.php';
-
-try{
-    $request =$db->prepare("SELECT * FROM question WHERE idquestion=? LIMIT 1");
-    $request->execute([$idquestion]);
-    $row = $request -> fetch();
+// try{
+//     $request =$db->prepare("SELECT * FROM question WHERE idquestion=? LIMIT 1");
+//     $request->execute([$idquestion]);
+//     $row = $request -> fetch();
     
-    $textQuestionDB =$row['text'];
-    $pictureQuestionDB =$row['picture'];
-    $proposal1 =$row['proposal_1'];
-    $proposal2 =$row['proposal_2'];
-    $proposal3 =$row['proposal_3'];
-    $proposalValid =$row['proposal_valid_index'];
-    echo "question recup";
-    var_dump($textQuestionDB);
+//     $textQuestionDB =$row['text'];
+//     $pictureQuestionDB =$row['picture'];
+//     $proposal1 =$row['proposal_1'];
+//     $proposal2 =$row['proposal_2'];
+//     $proposal3 =$row['proposal_3'];
+//     $proposalValid =$row['proposal_valid_index'];
+//     echo "question recup";
+//     var_dump($textQuestionDB);
 
-}
-catch(Exception $e){
+// }
+// catch(Exception $e){
     
-die("services momentanément indisponible. Merci de réessayer plus tard!!");
-}
+// die("services momentanément indisponible. Merci de réessayer plus tard!!");
+// }
 
-if($idquestion != $usernameDB){
-    header('location: index.php?page=login&msg=login_failed');
-    die();
-}
-
-
+// if($idquestion != $usernameDB){
+//     header('location: index.php?page=login&msg=login_failed');
+//     die();
+// }
 
 
 
-header('location: index.php?page=menu');
-die();
+
+
+// header('location: index.php?page=menu');
+// die();
